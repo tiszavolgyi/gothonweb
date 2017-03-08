59454c8eb32ff48c12421979e20886ebdff53a59
Peter Tiszavolgyi <tiszavolgyi@gmail.com>
2017-03-07 21:34:55 -0800

    Adding test2 url for testing git and do my first commit

---
+++ bin/app.py
@@ -0,0 +1,39 @@
+import web
+
+urls = (
+    '/', 'Index',
+    '/hello', 'Index',
+    '/test', 'Test',
+    '/hello2', 'Index2',
+    '/test2', 'Index2'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', base="layout")
+
+class Index(object):
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
+
+class Index2(object):
+    def GET(self):
+        form = web.input(greet="Hello", name="Nobody")
+        greeting2 = "%s, %s!" % (form.greet, form.name)
+        return render.index2(greeting2 = greeting2)
+
+
+class Test(object):
+    def GET(self):
+        copy = "Test Copy"
+        return render.test(copy = copy)
+
+
+if __name__ == "__main__":
+
+    app.run()
